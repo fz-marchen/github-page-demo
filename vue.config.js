97654ab72e64e50a03054d21128858bb7397b801
陈敬睿 <fz_marchen@163.com>
2025-10-20 17:27:18 +0800

    feat: 添加部署脚本

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/github-page-demo/' // 替换为你的 GitHub 仓库名
+    : '/',
   transpileDependencies: true
 })
