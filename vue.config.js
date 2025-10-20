d62193e1802716092cb8cadc5ddd86d27b15edea
陈敬睿 <fz_marchen@163.com>
2025-10-20 17:56:55 +0800

    chore: 构建test

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/github-page-demo/' // 替换为你的 GitHub 仓库名
-    : '/',
+  publicPath: '/github-page-demo/',
+  // process.env.NODE_ENV === 'production'
+  //   ? '/github-page-demo/' // 替换为你的 GitHub 仓库名
+  //   : '/',
   transpileDependencies: true
 })
