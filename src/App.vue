457f4e3c2e108ff3608a3af2ceec5c7405d9b6b3
陈敬睿 <fz_marchen@163.com>
2025-10-21 20:29:45 +0800

    feat: 删除文字

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <div>
-      hello github
-    </div>
+
   </div>
 </template>
 
